161e41bd08802a1fa7ecd9a1baa433ca951b742c
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-10 06:47:10 +0000

    Add restaurant database

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Database Config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Restaurants TABLE Configuration
+
+
+class Restaurants(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    eat_in = db.Column(db.Boolean, nullable=False)
+    delivers = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f'Restaurant Name: {self.name}'
+
+
+db.create_all()
 
 
 @app.route('/')
